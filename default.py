54127d697ddaed9e84d32dc9f9a57f5c2b0b78f6
NeverWise <matteopomi@libero.it>
2014-04-07 22:05:14 +0200

    Version 1.0.2 - Updated new live url. - Little improvements.

--- default.py
+++ default.py
@@ -1,4 +1,6 @@
-# Version 1.0.0 (16/09/2013)
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Version 1.0.2 (16/09/2013)
 # La Cosa
 # Organo di comunicazione Movimento 5 Stelle.
 # By NeverWise
@@ -17,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################
-import sys, re, xbmcplugin, tools
+import sys, re, xbmcplugin, tools#, datetime
 
 # Entry point.
 #startTime = datetime.datetime.now()
@@ -27,39 +29,41 @@ params = tools.urlParametersToDict(sys.argv[2])
 idPlugin = 'plugin.lacosa'
 
 if len(params) == 0: # Visualizzazione del menu.
-  response = tools.getResponseUrl('http://www.beppegrillo.it/la_cosa')
-
   # Diretta.
-  phpPage = re.compile('<div class="box_bottom">.+?<script src="(.+?)">').findall(response)[0]
-  img = re.compile('<div class="logo">.+?<img.+?src="(.+?)".+?/>').findall(response)[0]
-  live = tools.getResponseUrl(phpPage)
-  url = re.compile('{file: "(.+?)\.m3u8"}').findall(live)[0] + '.m3u8'
-  tools.addLink(handle, tools.getTranslation(idPlugin, 30001), img, '', 'video', { 'title' : 'La Cosa', 'plot' : '', 'duration' : -1, 'director' : '' }, url) # Diretta.
-
+  response = tools.getResponseUrl('http://www.beppegrillo.it/la_cosa')
+  if response != None:
+    phpPage = re.compile('<div class="box_bottom">.+?<script src="(.+?)">').findall(response)[0]
+    img = re.compile('<div class="logo">.+?<img.+?src="(.+?)".+?/>').findall(response)[0]
+    response = tools.getResponseUrl(phpPage)
+    if response != None:
+      url = re.compile('file: "(.+?)\.m3u8"').findall(response)[0] + '.m3u8'
+      tools.addLink(handle, tools.getTranslation(idPlugin, 30001), img, '', 'video', { 'title' : 'La Cosa', 'plot' : '', 'duration' : -1, 'director' : '' }, url) # Diretta.
   # Show.
-  show = re.compile('<div class="icon">(.+?)</div>').findall(response)[0]
-  show = re.compile('<a href="(.+?)"><img src="(.+?)" alt="(.+?)"/></a>').findall(show)
-  for link, img, title in show:
-    title = tools.normalizeText(title)
-    tools.addDir(handle, title, img, '', 'video', { 'title' : title, 'plot' : '', 'duration' : -1, 'director' : '' }, { 'id' : 's', 'page' : link })
+  response = tools.getResponseUrl('http://www.beppegrillo.it/la_cosa/rubriche')
+  if response != None:
+    show = re.compile('<div class="icon_programmi"><a href="(.+?)"><img.+?src="(.+?)".+?/></a><h3.+?><a.+?>(.+?)</a></h3><div.+?><p>(.+?)</p>').findall(response)
+    for link, img, title, descr in show:
+      title = tools.normalizeText(title)
+      tools.addDir(handle, title, img, '', 'video', { 'title' : title, 'plot' : tools.normalizeText(tools.stripTags(descr)), 'duration' : -1, 'director' : '' }, { 'id' : 's', 'page' : link })
   xbmcplugin.endOfDirectory(handle)
 else:
   response = tools.getResponseUrl(params['page'])
-  if params['id'] == 's': # Visualizzazione video di uno show.
-    response = re.compile('<div.+?id="recenti_canale">(.+?)<div class="pagination">').findall(response)[0]
-    response = re.compile('<a class="videoThumbnail" href="(.+?)"><img.+?src="(.+?)".+?/>.+?</a><h4><a.+?>(.+?)</a>').findall(response)
-    for link, img, title in response:
-      title = tools.normalizeText(title)
-      tools.addDir(handle, title, img, '', 'video', { 'title' : title, 'plot' : '', 'duration' : -1, 'director' : '' }, { 'id' : 'v', 'page' : link, 'image' : img })
-    xbmcplugin.endOfDirectory(handle)
-  elif params['id'] == 'v': # Riproduzione del video.
-    title = tools.normalizeText(re.compile('<h2 class="icon_article_left_title">(.+?)</h2>').findall(response)[0])
-    descr = re.compile('<div class="icon_article_left_txt">(.+?)</div>').findall(response)[0]
-    li = tools.createListItem(title, params['image'], '', 'video', { 'title' : title, 'plot' : tools.normalizeText(tools.stripTags(descr)), 'duration' : -1, 'director' : '' })
-    streams = re.compile("file: '(.+?)'").findall(response)
-    try:
-      xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).playStream(streams[0], li)
-    except:
-      xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).playStream(streams[1], li)
+  if response != None:
+    if params['id'] == 's': # Visualizzazione video di uno show.
+      response = re.compile('<div.+?id="recenti_canale">(.+?)<div class="pagination">').findall(response)[0]
+      response = re.compile('<a class="videoThumbnail" href="(.+?)"><img.+?src="(.+?)".+?/>.+?</a><h4><a title="(.+?)"').findall(response)
+      for link, img, title in response:
+        title = tools.normalizeText(title)
+        tools.addDir(handle, title, img, '', 'video', { 'title' : title, 'plot' : '', 'duration' : -1, 'director' : '' }, { 'id' : 'v', 'page' : link, 'image' : img })
+      xbmcplugin.endOfDirectory(handle)
+    elif params['id'] == 'v': # Riproduzione del video.
+      title = tools.normalizeText(re.compile('<h2 class="icon_article_left_title">(.+?)</h2>').findall(response)[0])
+      descr = re.compile('<div class="icon_article_left_txt">(.+?)</div>').findall(response)[0]
+      li = tools.createListItem(title, params['image'], '', 'video', { 'title' : title, 'plot' : tools.normalizeText(tools.stripTags(descr)), 'duration' : -1, 'director' : '' })
+      streams = re.compile("file: '(.+?)'").findall(response)
+      try:
+        xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).playStream(streams[0], li)
+      except:
+        xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).playStream(streams[1], li)
 
 #print 'La cosa azione ' + str(datetime.datetime.now() - startTime)
